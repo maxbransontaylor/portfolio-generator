fce813da48036616bafb0854c9a9fcaafbebf0c3
Max Taylor <maxbransontaylor@gmail.com>
2022-07-24 17:43:41 -0400

    bug

--- app.js
+++ app.js
@@ -1,9 +1,94 @@
+const inquirer = require("inquirer");
 const fs = require("fs");
 const generatePage = require("./src/page-template.js");
 const profileDataArgs = process.argv.slice(2);
 const [name, github] = profileDataArgs;
 
-fs.writeFile("index.html", generatePage(name, github), (err) => {
-  if (err) throw err;
-  console.log("Portfolio complete! Check out index.html to see the output!");
-});
+// fs.writeFile("index.html", generatePage(name, github), (err) => {
+//   if (err) throw err;
+//   console.log("Portfolio complete! Check out index.html to see the output!");
+// });
+
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is your name?",
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "Enter your Github Username",
+    },
+    {
+      type: "input",
+      name: "about",
+      message: "Provide some information about yourself:",
+    },
+  ]);
+};
+const promptProject = (portfolioData) => {
+  console.log(`
+  =================
+  Add a New Project
+  =================
+  `);
+  //why error when portfolioData.projects?
+  if (!portfolioData.projects) {
+    portfolioData.projects = [];
+  }
+  //why two return statements in this function?
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the name of your project?",
+      },
+      {
+        type: "input",
+        name: "description",
+        message: "Provide a description of your project (Required)",
+      },
+      {
+        type: "checkbox",
+        name: "languages",
+        message: "What did you build this project with? (check all that apply)",
+        choices: [
+          "JavaScript",
+          "Html",
+          "CSS",
+          "ES6",
+          "jQuery",
+          "Bootstrap",
+          "Node",
+        ],
+      },
+      {
+        type: "confirm",
+        name: "feature",
+        message: "Would you like to feature this project?",
+        default: false,
+      },
+      {
+        type: "confirm",
+        name: "confirmAddProject",
+        message: "Would you like to enter another project?",
+        default: false,
+      },
+    ])
+    .then((projectData) => {
+      portfolioData.projects.push(projectData);
+      if (projectData.confirmAddProject) {
+        //why two return statements in this function?
+        return promptProject(portfolioData);
+      } else {
+        return portfolioData;
+      }
+    });
+};
+promptUser()
+  .then((answers) => console.log(answers))
+  .then(promptProject)
+  .then((portfolioData) => console.log(portfolioData));
