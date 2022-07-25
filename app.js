5799e68787c538a5b41efce3ea32eec07ae6b26b
Max Taylor <maxbransontaylor@gmail.com>
2022-07-25 17:15:12 -0400

    finish with asynchronus functions

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const generatePage = require("./src/page-template.js");
-const profileDataArgs = process.argv.slice(2);
-const [name, github] = profileDataArgs;
-
-// fs.writeFile("index.html", generatePage(name, github), (err) => {
-//   if (err) throw err;
-//   console.log("Portfolio complete! Check out index.html to see the output!");
-// });
-
+const { writeFile, copyFile } = require("./utils/generate-site.js");
 const promptUser = () => {
   return inquirer.prompt([
     {
@@ -147,65 +140,22 @@ const promptProject = (portfolioData) => {
       }
     });
 };
-// promptUser()
-//   .then(promptProject)
-//   .then((portfolioData) => {
-//     const pageHTML = generatePage(portfolioData);
-//     // fs.writeFile("./index.html", pageHTML, (err) => {
-//     //   if (err) throw new Error(err);
-//     //   console.log(
-//     //     "Page created! Check out index.html in this directory to see it!"
-//     //   );
-//     // });
-//   });
-
-const mockData = {
-  name: "Lernantino",
-  github: "lernantino",
-  confirmAbout: true,
-  about:
-    "Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et.",
-  projects: [
-    {
-      name: "Run Buddy",
-      description:
-        "Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.",
-      languages: ["HTML", "CSS"],
-      link: "https://github.com/lernantino/run-buddy",
-      feature: true,
-      confirmAddProject: true,
-    },
-    {
-      name: "Taskinator",
-      description:
-        "Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.",
-      languages: ["JavaScript", "HTML", "CSS"],
-      link: "https://github.com/lernantino/taskinator",
-      feature: true,
-      confirmAddProject: true,
-    },
-    {
-      name: "Taskmaster Pro",
-      description:
-        "Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.",
-      languages: ["JavaScript", "jQuery", "CSS", "HTML", "Bootstrap"],
-      link: "https://github.com/lernantino/taskmaster-pro",
-      feature: false,
-      confirmAddProject: true,
-    },
-    {
-      name: "Robot Gladiators",
-      description:
-        "Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque.",
-      languages: ["JavaScript"],
-      link: "https://github.com/lernantino/robot-gladiators",
-      feature: false,
-      confirmAddProject: false,
-    },
-  ],
-};
-
-const pageHTML = generatePage(mockData);
-fs.writeFile("index.html", pageHTML, (err) => {
-  if (err) throw err;
-});
+//don't understand what makes this asynchronus, doesn't it all run in order either way?
+promptUser()
+  .then(promptProject)
+  .then((portfolioData) => {
+    return generatePage(portfolioData);
+  })
+  .then((pageHTML) => {
+    return writeFile(pageHTML);
+  })
+  .then((writeFileResponse) => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then((copyFileResponse) => {
+    console.log(copyFileResponse);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
